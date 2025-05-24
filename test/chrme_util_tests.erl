9e2e78260d476f7455516c0cd45d4184d4f98e02
ts-klassen <git@su-shiki.com>
2025-05-25 03:32:46 +0900

    added tests

---
+++ test/chrme_util_tests.erl
@@ -0,0 +1,15 @@
+%% Simple EUnit tests for chrme_util module
+-module(chrme_util_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+maybe_to_list_test() ->
+    ?assertEqual("hello", chrme_util:maybe_to_list(<<"hello">>)),
+    ?assertEqual("world", chrme_util:maybe_to_list("world")).
+
+maybe_to_binary_test() ->
+    ?assertEqual(<<"hello">>, chrme_util:maybe_to_binary("hello")),
+    ?assertEqual(<<"world">>, chrme_util:maybe_to_binary(<<"world">>)).
+
+parse_ws_url_test() ->
+    ?assertEqual({<<"example.com">>,80,<<"/path">>},
+                 chrme_util:parse_ws_url("ws://example.com:80/path")).
